4efa2cdac7490dfe16a1e91ff52de9984eb35b6f
Eetu Suihkonen <eetu.suihkonen@gmail.com>
2021-12-29 21:51:21 +0200

    Install only needed packages

--- conf.bash
+++ conf.bash
@@ -1,6 +1,6 @@
 REPOS="void-repo-multilib void-repo-nonfree void-repo-multilib-nonfree"
 
-PACKAGES="base-system grub-x86_64-efi seatd socklog-void bluez zramen
+PACKAGES="base-system grub-x86_64-efi seatd socklog-void zramen
 
           curl git rsync
           neovim nnn ripgrep scc
@@ -12,11 +12,8 @@ PACKAGES="base-system grub-x86_64-efi seatd socklog-void bluez zramen
           fuse-sshfs jmtpfs
           nmap binwalk strace bind-utils libva-utils
 
-          light iwd intel-video-accel igt-gpu-tools mesa-vulkan-intel mesa-vulkan-intel-32bit intel-undervolt
-          nvidia nvidia-libs-32bit nvtop
-
           mesa-dri vulkan-loader Vulkan-Tools mesa-demos glxinfo
-          pipewire libspa-bluetooth pulsemixer pulseaudio-utils
+          pipewire pulsemixer pulseaudio-utils
           xdg-utils terminus-font
           sway wev i3status j4-dmenu-desktop
           alacritty grimshot mpv zathura-pdf-mupdf scrcpy
@@ -35,4 +32,15 @@ FIREFOX="ublock-origin universal-bypass proxy-toggle
          new-tab-override matte-black-red"
 
 
-SERVICES="acpid socklog-unix nanoklogd seatd zramen dbus iwd bluetoothd intel-undervolt"
+SERVICES="acpid socklog-unix nanoklogd seatd zramen dbus"
+
+shopt -s expand_aliases
+alias M="lsmod | grep -q"
+A() { V=$1; shift; eval "$V=\"${!V} $*\"" ;}
+alias AP="A PACKAGES"
+alias AS="A SERVICES"
+
+M     iwlwifi           && { AP iwd;                                                                              AS iwd             ;}
+M     btusb             && { AP bluez libspa-bluetooth;                                                           AS bluetoothd      ;}
+M     i915              && { AP light intel-video-accel igt-gpu-tools mesa-vulkan-intel{,-32bit} intel-undervolt; AS intel-undervolt ;}
+M -E '(nouveau|nvidia)' &&   AP nvidia nvidia-libs-32bit nvtop
