387df96e64ff86112e72c3e58f8da271696ebdf9
Eetu Suihkonen <eetu.suihkonen@gmail.com>
2021-12-30 10:36:54 +0200

    Add liquidctl (if needed)

--- conf.bash
+++ conf.bash
@@ -5,7 +5,7 @@ PACKAGES="base-system grub-x86_64-efi seatd socklog-void
           curl git rsync
           neovim nnn ripgrep scc
           htop lm_sensors
-          gcc pkg-config rustup rust-analyzer go pnpm shellcheck
+          gcc pkg-config rustup rust-analyzer go pnpm python3-pip shellcheck
 
           bash-completion xtools chrony python3-Twisted pngquant
           zip unzip lz4 zstd squashfs-tools
@@ -26,6 +26,7 @@ PACKAGES="base-system grub-x86_64-efi seatd socklog-void
 SRC="discord spotify"
 
 CARGO="ncspot" PACKAGES="$PACKAGES dbus-devel libxcb-devel openssl-devel ncurses-devel"
+PIP=
 
 FIREFOX="ublock-origin universal-bypass proxy-toggle
          cliget vimium-ff simple-translate single-file
@@ -36,6 +37,8 @@ SERVICES="acpid socklog-unix nanoklogd zram seatd dbus"
 
 shopt -s expand_aliases
 alias M="lsmod | grep -q"
+alias U="lsusb | grep -q"
+
 A() { V=$1; shift; eval "$V=\"${!V} $*\"" ;}
 alias AP="A PACKAGES"
 alias AS="A SERVICES"
@@ -44,3 +47,4 @@ M     iwlwifi           && { AP iwd;                                            
 M     btusb             && { AP bluez libspa-bluetooth;                                                           AS bluetoothd      ;}
 M     i915              && { AP light intel-video-accel igt-gpu-tools mesa-vulkan-intel{,-32bit} intel-undervolt; AS intel-undervolt ;}
 M -E '(nouveau|nvidia)' &&   AP nvidia nvidia-libs-32bit nvtop
+U     NZXT              && { AP python3-{devel,setuptools,docopt,usb} libusb;                                     A PIP liquidctl    ;}
